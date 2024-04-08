2addf732a769394132ac3da119bcf0e6472e1e65
ongweny <ongwenyjoel@gmail.com>
2024-04-08 09:16:14 +0300

    complete

---
+++ index.js
@@ -0,0 +1,7 @@
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+function logTwoValues(name, language) {
+    console.log(`The two values are ${name} and ${language}.`);
+  }
